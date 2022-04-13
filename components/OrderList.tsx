85767366ab533ddae576ea7f433bf7c8b2352f91
Richard Sönnerberg <31686265+sonnerberg@users.noreply.github.com>
2022-04-13 11:46:52 +0200

    Refactor and use order model

--- components/OrderList.tsx
+++ components/OrderList.tsx
@@ -1,15 +1,14 @@
 import { useState, useEffect } from "react";
 import { View, Text, Button } from "react-native";
 import { api_key, base_url } from "../config/config.json";
+import orders from "../models/orders";
 
 const OrderList = ({ route, navigation }) => {
   const { reload } = route.params || false;
   const [allOrders, setAllOrders] = useState([]);
 
   const fetchAllOrders = async () => {
-    const response = await fetch(`${base_url}/orders?api_key=${api_key}`);
-    const result = await response.json();
-    setAllOrders(result.data);
+    setAllOrders(await orders.getOrders());
   };
 
   if (reload) {
